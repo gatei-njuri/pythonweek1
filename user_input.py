eaa0cf46c3b529de7bcc056385bc19ec6df6d6d0
gatei-njuri <157600369+gatei-njuri@users.noreply.github.com>
2024-03-04 03:15:15 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name= input('Enter name: ')
+age= input('Enter age: ')
+location= input('Enter your location: ')
+u= name +age + location
+print(u)
+print("Hello", name ,"you are", age ,"years old and live in", location)
+
